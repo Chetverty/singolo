edd13c5d08b3074f4875f093cefc498f12251a0d
Fedor <flask345@gmail.com>
2020-03-16 22:42:58 +0300

    feat: add active elements of the menu and links for navigation bar

---
+++ script.js
@@ -0,0 +1,21 @@
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+var nav = document.getElementById("hor-menu-id");
+var items = nav.getElementsByClassName("hor-menu-item");
+for (var i = 0; i < items.length; i++) {
+    items[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
